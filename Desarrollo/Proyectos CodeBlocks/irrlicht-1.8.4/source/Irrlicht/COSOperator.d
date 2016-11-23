0214008832aa0473438649f5c68da938d1dbeebb
Juan Sánchez Almendro <juansanalme@gmail.com>
2016-11-23 15:50:13 +0100

    Irrlicht ejemplo 2
    Ya no hace falta que tengais iirlicht en vuestro equipo, esta
    referenciando al del github

---
+++ Desarrollo/Proyectos CodeBlocks/irrlicht-1.8.4/source/Irrlicht/COSOperator.d
@@ -0,0 +1,4 @@
+COSOperator.o: COSOperator.cpp COSOperator.h ../../include/IOSOperator.h \
+ ../../include/IReferenceCounted.h ../../include/irrTypes.h \
+ ../../include/IrrCompileConfig.h ../../include/irrString.h \
+ ../../include/irrAllocator.h ../../include/irrMath.h
