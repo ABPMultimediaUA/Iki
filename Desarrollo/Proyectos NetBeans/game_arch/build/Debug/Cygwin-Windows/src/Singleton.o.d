4d2b086206389b445c752c22ab5080f913448e45
Juan Sánchez Almendro <juansanalme@gmail.com>
2016-12-19 23:19:21 +0100

    Cosas Varias
    - Añadidos los bocetos ( quizas caiga alguno mas )
    - Movido la documentacion de sitio
    - Otro tipo de mierdas

--- Desarrollo/Proyectos NetBeans/game_arch/build/Debug/Cygwin-Windows/src/Singleton.o.d
+++
@@ -1,4 +0,0 @@
-build/Debug/Cygwin-Windows/src/Singleton.o: src/Singleton.cpp \
- src/Singleton.h
-
-src/Singleton.h:
--- Desarrollo/Proyectos NetBeans/game_arch/build/Debug/Cygwin-Windows/src/State.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/Cygwin-Windows/src/State.o: src/State.cpp src/State.h \
- src/Singleton.h src/Menu.h
-
-src/State.h:
-
-src/Singleton.h:
-
-src/Menu.h:
