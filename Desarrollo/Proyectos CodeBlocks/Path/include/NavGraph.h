8dc53964f8de6a32786887b534560c55cc14b4fb
Mateo <mateobernal19@gmail.com>
2017-02-07 16:06:32 +0100

    comit path

---
+++ Desarrollo/Proyectos CodeBlocks/Path/include/NavGraph.h
@@ -0,0 +1,19 @@
+#ifndef NAVGRAPH
+#define NAVGRAPH
+
+#include <irrlicht.h>
+#include <enemigo.h>
+
+class NavGraph
+{
+    public:
+        NavGraph();
+        virtual ~NavGraph();
+
+
+    protected:
+
+    private:
+};
+
+#endif // NAVGRAPH
