d611d8646261ce98374a4f7f2d407104c898d346
Pablo Campos <port.d.ace@hotmail.com>
2017-03-29 10:43:36 +0200

    Cambios en MASTER para enseñar a Fran

--- Desarrollo/Proyectos CodeBlocks/Juego/src/GUI/GUI_Button.cpp
+++ Desarrollo/Proyectos CodeBlocks/Juego/src/GUI/GUI_Button.cpp
@@ -7,28 +7,6 @@ GUI_Button::GUI_Button(int x, int y, std::string cadena)
     cadena = "resources/texturas/"+cadena+".png";
     const char * c = cadena.c_str();
     image = new Image(c);
-    /*switch(t){
-    case 0:
-        image = new Image("resources/texturas/nuevapartidaboton.png");
-        break;
-    case 1:
-        image = new Image("resources/texturas/salirboton.png");
-        break;
-    case 2:
-        image = new Image("resources/texturas/botonopciones.png");
-        break;
-    case 3:
-        image = new Image("resources/texturas/botoncontinuar.png");
-        break;
-    case 4:
-        image = new Image("resources/texturas/botonatras.png");
-        break;
-    case 5:
-        image = new Image("resources/texturas/aceptarboton.png");
-        break;
-    default:
-        break;
-    }*/
 
     posicionX = x;
     posicionY = y;
