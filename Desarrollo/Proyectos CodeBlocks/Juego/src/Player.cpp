15299c56a9ff906f2988747eadc0c827625b898d
Juan Sánchez Almendro <juansanalme@gmail.com>
2017-05-16 10:56:48 +0200

    cambios en waypoints

--- Desarrollo/Proyectos CodeBlocks/Juego/src/Player.cpp
+++ Desarrollo/Proyectos CodeBlocks/Juego/src/Player.cpp
@@ -79,8 +79,8 @@ void Player::MoverPlayer(Structs::TPosicion p1,Structs::TPosicion p2){
 }
 void Player::moverBody(Structs::TPosicion vec){
     vec.Normalize();
-    float movx = vec.X * avMovement * velocidad ;
-    float movy = vec.Z * avMovement * velocidad ;
+    float movx = vec.X * avMovement * velocidad *0.75;
+    float movy = vec.Z * avMovement * velocidad *0.75;
 
     if (vec == quietoParado){
         isMoving = false;
